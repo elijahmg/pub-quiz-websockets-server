0ae5270da837e17e4807d0f1160802ac41053189
Ilya <elijahmg25@gmail.com>
2023-05-30 15:11:48 +0200

    feat: sql return structure correspond to quiz state

--- sql-utils/quiz-status-update.sql
+++ sql-utils/quiz-status-update.sql
@@ -1,10 +1,11 @@
 -- Creates trigger for local development
+-- Data shape must correspond to useTeamQuizDataStore
 CREATE or REPLACE FUNCTION public.quiz_status_trigger_function() returns trigger AS $$
 Declare
     begin
         perform pg_notify(cast('update_quiz_status' AS text), json_build_object(
 	'id', qB.id, 'status', qB.status,
-	'currentQuestion', json_build_object('id', q.id, 'content', q.content, 'currentRound', json_build_object('id', r.id, 'name', r.name)))::text)
+	'currentQuestion', json_build_object('id', q.id, 'content', q.content, 'mediaType', q."mediaType", 'mediaURL', q."mediaURL", 'round', json_build_object('id', r.id, 'name', r.name)))::text)
 from "QuizStatus" qB
 inner join public."Question" q on qB."currentQuestionId" = q.id
 inner join public."Round" r on q."roundId" = r.id
